110267ee0a2216710a992b499a6ac8877af38994
Bill Menees <Bill@Menees.com>
2022-03-22 21:19:40 -0500

    Stubbing out Xaml form

---
+++ src/RegExponent/Commands.cs
@@ -0,0 +1,28 @@
+﻿namespace RegExponent
+{
+	#region Using Directives
+
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Input;
+
+	#endregion
+
+	internal static class Commands
+	{
+		#region Public Fields
+
+		public static readonly RoutedUICommand About = new(nameof(About), nameof(About), typeof(Commands));
+
+		public static readonly RoutedUICommand Exit = new(
+			nameof(Exit),
+			nameof(Exit),
+			typeof(Commands),
+			new InputGestureCollection { new KeyGesture(Key.F4, ModifierKeys.Alt), });
+
+		#endregion
+	}
+}
--- src/RegExponent/MainWindow.xaml.cs
+++ src/RegExponent/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿namespace RegExponent
 {
+	#region Using Directives
+
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
@@ -14,15 +16,66 @@
 	using System.Windows.Media.Imaging;
 	using System.Windows.Navigation;
 	using System.Windows.Shapes;
+	using Menees;
+	using Menees.Windows.Presentation;
+
+	#endregion
 
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow
 	{
+		#region Private Data Members
+
+		private readonly WindowSaver saver;
+
+		#endregion
+
+		#region Constructors
+
 		public MainWindow()
 		{
 			this.InitializeComponent();
+			this.saver = new WindowSaver(this);
+			this.saver.LoadSettings += this.Saver_LoadSettings;
+			this.saver.SaveSettings += this.Saver_SaveSettings;
 		}
+
+		#endregion
+
+		#region Private Event Handlers
+
+		private void ExitExecuted(object? sender, ExecutedRoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		private void HelpExecuted(object? sender, ExecutedRoutedEventArgs e)
+		{
+			if (e.Parameter is string text && Uri.TryCreate(text, UriKind.Absolute, out Uri? uri))
+			{
+				WindowsUtility.ShellExecute(this, uri.ToString());
+			}
+		}
+
+		private void AboutExecuted(object sender, RoutedEventArgs e)
+		{
+			WindowsUtility.ShowAboutBox(this, this.GetType().Assembly, nameof(RegExponent));
+		}
+
+		private void Saver_LoadSettings(object? sender, SettingsEventArgs e)
+		{
+			// TODO: Finish Saver_LoadSettings. [Bill, 3/22/2022]
+			Conditions.RequireReference(this, nameof(MainWindow));
+		}
+
+		private void Saver_SaveSettings(object? sender, SettingsEventArgs e)
+		{
+			// TODO: Finish Saver_SaveSettings. [Bill, 3/22/2022]
+			Conditions.RequireReference(this, nameof(MainWindow));
+		}
+
+		#endregion
 	}
 }
