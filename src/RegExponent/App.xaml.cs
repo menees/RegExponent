5403dafb5a5ae250b3ddf65fac981e6e99b9ec47
Bill Menees <Bill@Menees.com>
2022-05-09 17:38:27 -0500

    Working on Pattern highlighter

--- src/RegExponent/App.xaml.cs
+++ src/RegExponent/App.xaml.cs
@@ -45,22 +45,17 @@
 
 		private static void RegisterHighlighting(string highlightName)
 		{
-			IHighlightingDefinition definition = LoadHighlightingDefinition(highlightName + ".xshd");
-
-			// Based on https://stackoverflow.com/a/46512591/1882616
-			HighlightingManager.Instance.RegisterHighlighting(highlightName, Array.Empty<string>(), definition);
-		}
-
-		private static IHighlightingDefinition LoadHighlightingDefinition(string resourceName)
-		{
 			// Based on https://stackoverflow.com/a/5057464/1882616.
-			string fullName = $"{nameof(RegExponent)}.Highlights.{resourceName}";
-			using (Stream stream = typeof(App).Assembly.GetManifestResourceStream(fullName)
-				?? throw Exceptions.NewInvalidOperationException($"Could not find resource {fullName}."))
+			string resourceName = $"{nameof(RegExponent)}.Highlights.{highlightName}.xshd";
+
+			using (Stream stream = typeof(App).Assembly.GetManifestResourceStream(resourceName)
+				?? throw Exceptions.NewInvalidOperationException($"Could not find resource {resourceName}."))
 			using (XmlReader reader = XmlReader.Create(stream))
 			{
-				IHighlightingDefinition result = HighlightingLoader.Load(reader, HighlightingManager.Instance);
-				return result;
+				// Based on https://stackoverflow.com/a/46512591/1882616
+				HighlightingManager highlighters = HighlightingManager.Instance;
+				IHighlightingDefinition definition = HighlightingLoader.Load(reader, highlighters);
+				highlighters.RegisterHighlighting(highlightName, Array.Empty<string>(), definition);
 			}
 		}
 
